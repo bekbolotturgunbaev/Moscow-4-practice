4af3d2a4e9440b4b40f8a77e7d285416da84a808
bekbolotturgunbaev <bekaturgunbaev007@gmail.com>
2022-09-23 22:36:52 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+   /*     Scanner scanner = new Scanner(System.in);
+         int a = scanner.nextInt();
+         byte b =scanner.nextByte();
+
+         System.out.println(a+10);
+         System.out.println(b+10); */
+
+        Scanner sc = new Scanner(System.in);
+
+
+       /* int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println((a)/(double)b); */
+
+     /*   int a = sc.nextInt();
+        int b = 1999;
+        int c = 2022;
+        System.out.println(b - c); */
+
+       /* int a = 1000000;
+        float f = 0.2f;
+
+
+        System.out.println((a) - (double)f); */
+
+        int a = 23;
+        int b = 20;
+        int c =sc.nextInt();
+        int d =sc.nextInt();
+
+        System.out.println("bul menin jashym - "  + a);
+        System.out.println("bul menin jashym emes - " + b);
+
+
+
+    }
+}
